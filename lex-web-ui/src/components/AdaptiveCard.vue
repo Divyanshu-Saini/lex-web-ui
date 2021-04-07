ee32e9337584d8a9931e28d17f4aea30fb6fa2ed
Divyanshu <divyanshu.saini@ndsglobal.com>
2021-04-07 11:24:47 +0530

    Updated Adaptive card data binding

--- lex-web-ui/src/components/AdaptiveCard.vue
+++ lex-web-ui/src/components/AdaptiveCard.vue
@@ -11,44 +11,46 @@
 <script>
 /* eslint no-underscore-dangle: 0 */
 /* eslint-disable */
-import { AdaptiveCards } from 'adaptivecards-vue';
-import * as aConfig from '../config/adaptive.card.template.json';
+import { AdaptiveCards } from "adaptivecards-vue";
+// import * as aConfig from '../config/adaptive.card.template.json';
 
 export default {
-  name: 'adaptive-card',
-  props: ['customPayload'],
+  name: "adaptive-card",
+  props: ["customPayload"],
   components: {
     AdaptiveCards,
   },
   data() {
     return {
-      data: '',
-      card: '',
-      config: '',
+      data: "",
+      card: "",
+      config: "",
     };
   },
   methods: {
     onItemClick(event) {
       const message = {
-        type: 'human',
+        type: "human",
         text: JSON.stringify(event._processedData),
         event: event._processedData,
       };
 
-      this.$store.dispatch('postTextMessage', message);
+      this.$store.dispatch("postTextMessage", message);
     },
   },
   created() {
-    console.info('In adaptiveCards :', this.customPayload);
-    console.info('Adavtive jsons', aConfig);
-    const acard = aConfig.filter(ele => ele.id === this.customPayload.customPayload.id)[0];
-    console.info('acard :', acard);
-    this.data =
-      typeof this.customPayload.customPayload.data === 'object'
+    console.info("In adaptiveCards :", this.customPayload);
+    if (this.customPayload.isCustom && this.customPayload.customPayload) {
+      this.card = this.customPayload.customPayload.card
+        ? this.customPayload.customPayload.card
+        : "";
+      this.data = this.customPayload.customPayload.data
         ? this.customPayload.customPayload.data
-        : acard.data;
-    this.card = acard.card;
-    this.config = acard.config;
+        : "";
+      this.config = this.customPayload.customPayload.config
+        ? this.customPayload.customPayload.config
+        : "";
+    }
   },
 };
 </script>
